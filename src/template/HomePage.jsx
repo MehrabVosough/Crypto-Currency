8117c285060b7bf14b66c00d83995f9548b8c6e6
Mehrab vosough <mehrabvosough.dp@gmail.com>
2024-02-07 11:23:11 +0330

    Pagination added

--- src/template/HomePage.jsx
+++ src/template/HomePage.jsx
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { GetCoinList } from "../services/GetCoinList";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
 	const [coin, setCoine] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [page, setPage] = useState(1);
 
 	useEffect(() => {
+		setIsLoading(true);
 		const getData = async () => {
-			const res = await fetch(GetCoinList());
+			const res = await fetch(GetCoinList(page));
 			const json = await res.json();
 			setCoine(json);
 			setIsLoading(false);
 		};
 		getData();
-	}, []);
+	}, [page]);
 
-	return <TableCoin coin={coin} isLoading={isLoading} />;
+	return (
+		<div>
+			<TableCoin coin={coin} isLoading={isLoading} />
+			<Pagination page={page} setPage={setPage} />
+		</div>
+	);
 }
 
 export default HomePage;
